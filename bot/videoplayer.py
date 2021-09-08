e43ffe550dad49a14b169a35ce746d0e3d833ee6
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-09-08 21:30:29 +0600

    Update videoplayer.py

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -1,8 +1,7 @@
 import os
 import re
-import pafy
-from pyyoutube import ytdl
 import asyncio
+from pyyoutube import ytdl
 from pytgcalls import GroupCallFactory
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -28,8 +27,8 @@ async def stream(client, m: Message):
             youtube_regex_match = re.match(youtube_regex, video)
             if youtube_regex_match:
             	try:
-            	        video_url = ytdl(video).besturl()
-        	except Exception as e:
+            		video_url = ytdl(video).besturl()
+            	except Exception as e:
             		await m.reply(f"**Error** -- `{e}`")
             		return
             	msg = await m.reply("`Starting Live Stream...`")
